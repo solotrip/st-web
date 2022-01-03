09a7c1f24fa80449333922fd77cb300c836131ca
Fatih Tas <fatihtas26@gmail.com>
2022-01-03 03:20:51 +0300

    Remove apple sign in button on android

--- src/features/auth/components/login.jsx
+++ src/features/auth/components/login.jsx
@@ -7,6 +7,7 @@ import { MdChevronRight } from 'react-icons/md'
 import styles from './common-styles.module.scss'
 import { ReactComponent as GoogleIcon } from 'assets/images/google.svg'
 import { ReactComponent as AppleIcon } from 'assets/images/apple.svg'
+import { Capacitor } from '@capacitor/core'
 
 const LoginPage = ({ loginFunc, error, loginWithApple, loginWithGoogle }) => {
   const onLogin = ({ email, password }) => {
@@ -64,6 +65,7 @@ const LoginPage = ({ loginFunc, error, loginWithApple, loginWithGoogle }) => {
             <GoogleIcon/>
             <span>Sign in with Google</span>
           </button>
+          {Capacitor.getPlatform() !== 'android' && (
           <button
             className={cn(styles.socialLogin, styles.loginWithApple)}
             onClick={loginWithApple}
@@ -71,6 +73,7 @@ const LoginPage = ({ loginFunc, error, loginWithApple, loginWithGoogle }) => {
             <AppleIcon/>
             <span>Sign in with Apple</span>
           </button>
+          )}
         </div>
       </div>
     </div>
