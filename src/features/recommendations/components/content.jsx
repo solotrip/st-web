4f88e37db2d38827c05f40bdff331d549b2c0382
Berk Durmus <berkdurmus@yahoo.com>
2022-01-22 00:57:20 +0300

    Prevent Mapbox from Downloading Resources on Mobile
    Prevent Mapbox from Downloading Resources on Mobile

--- src/features/recommendations/components/content.jsx
+++ src/features/recommendations/components/content.jsx
@@ -9,10 +9,12 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import { Loader } from 'components'
 import RecommendationDetails from './recommendation-details'
 import { useHistory } from 'react-router-dom'
-
-const Map = ReactMapboxGl({
-  accessToken: MAPBOX_TOKEN
-})
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from 'react-device-detect'
 
 const Content = ({
   recommendations,
@@ -31,13 +33,17 @@ const Content = ({
 }) => {
   const [appTheme] = useThemeState()
   const history = useHistory()
-  //default dark map.
+
+  const Map =
+    isBrowser &&
+    ReactMapboxGl({
+      accessToken: MAPBOX_TOKEN
+    })
+
+  const [focusLocation, setFocusLocation] = useState([-0.118092, 51.509865])
   const [mapboxTheme, setMapboxTheme] = useState(
     'mapbox://styles/naberk/ckxnlqnws136z14qrjz7upcaj'
   )
-
-  const [focusLocation, setFocusLocation] = useState([-0.118092, 51.509865])
-
 
   const itemsRef = useRef([])
 
@@ -57,7 +63,7 @@ const Content = ({
   useEffect(
     () => {
       itemsRef.current = itemsRef.current.slice(0, recommendations.length)
-      if(recommendations.length > 0) {
+      if (recommendations.length > 0) {
         activeHandler(recommendations[0])
       }
     },
@@ -66,119 +72,112 @@ const Content = ({
 
   useEffect(
     () => {
-      appTheme === 'light'
-        ? setMapboxTheme('mapbox://styles/naberk/ckxnlqnws136z14qrjz7upcaj')
-        : setMapboxTheme('mapbox://styles/naberk/ckxnlqnws136z14qrjz7upcaj')
+      const { query } = queryFunction()
+      if (query && query.lon && query.lat) {
+        setFocusLocation([parseFloat(query.lon), parseFloat(query.lat)])
+      }
     },
-    [appTheme]
+    [queryFunction]
   )
 
-  useEffect(    () => {
-    const { query } = queryFunction()
-    if (query && query.lon && query.lat) {
-      setFocusLocation([parseFloat(query.lon), parseFloat(query.lat)])
-    }
-  },
-  [queryFunction]
-  )
+  if (loading) return <Loader />
 
-  if (loading) return <Loader/>
-
-  const list = (<div className={styles.recommendations}>
-
+  const list = (
+    <div className={styles.recommendations}>
       {title && <h1 className={styles.title}>{title}</h1>}
       {children}
       {!loading &&
-      recommendations.length === 0 && (
-        <span className={styles.noItems}>{noItemsMessage}</span>
+        recommendations.length === 0 && (
+          <span className={styles.noItems}>{noItemsMessage}</span>
       )}
 
       {!loading &&
-      recommendations.length > 0 &&
-      recommendations.map((recommendation, i) => {
-        const { query, queryString } = queryFunction(recommendation)
-        return (
-          <Recommendation
-            key={`rec-${recommendation.sid}`}
-            refHolder={el => (itemsRef.current[i] = el)}
-            query={query}
-            queryString={queryString}
-            recommendation={recommendation}
-            user={user}
-            activeHandler={() => activeHandler(recommendation)}
-            toggleWishlist={toggleWishlist}
-            wishlisted={!!wishlistedIds[recommendation.id]}
-            basePath={basePath}
-          />
-        )
-      })}
+        recommendations.length > 0 &&
+        recommendations.map((recommendation, i) => {
+          const { query, queryString } = queryFunction(recommendation)
+          return (
+            <Recommendation
+              key={`rec-${recommendation.sid}`}
+              refHolder={el => (itemsRef.current[i] = el)}
+              query={query}
+              queryString={queryString}
+              recommendation={recommendation}
+              user={user}
+              activeHandler={() => activeHandler(recommendation)}
+              toggleWishlist={toggleWishlist}
+              wishlisted={!!wishlistedIds[recommendation.id]}
+              basePath={basePath}
+            />
+          )
+        })}
     </div>
   )
 
   return (
     <div className={styles.page}>
-
-      {detailIndex === -1 ? list : (
+      {detailIndex === -1 ? (
+        list
+      ) : (
         <div className={styles.recommendationDetails}>
           <RecommendationDetails
             recommendation={recommendations[detailIndex]}
-            passports={queryFunction(
-              recommendations[detailIndex]
-            ).query.passports}
-          /></div>)
-      }
-
-      {mapEnabled && (
-        <div className={styles.mapbox}>
-          <Map
-            style={mapboxTheme}
-            containerStyle={{
-              height: '100vh',
-              width: '100%'
-            }}
-            center={
-              [focusLocation[0], focusLocation[1]]
+            passports={
+              queryFunction(recommendations[detailIndex]).query.passports
             }
-            zoom={[10]}
-            pitch={[30]}
-          >
-            {!loading &&
-            recommendations.length > 0 &&
-            recommendations.map((recommendation, index) => (
-              <div key={recommendation.lon}>
-                <Marker
-                  coordinates={{
-                    lng: recommendation.lon,
-                    lat: recommendation.lat
-                  }}
-                  className={styles.mapMarker}
-                />
-
-                <Popup
-                  offset={[0, 0]}
-                  coordinates={{
-                    lng: recommendation.lon,
-                    lat: recommendation.lat
-                  }}
-                >
-                  <button
-                    onClick={() => openDetails(recommendation)}
-                    className={styles.popupContent}
-                  >
-                    <div className={styles.colorStrip}/>
-                    <div className={styles.popupContent2}>
-                      <div className={styles.popupIndex}> {index + 1}</div>
-
-                      <div className={styles.popupInner}>
-                        {recommendation.name}
-                      </div>
-                    </div>
-                  </button>
-                </Popup>
-              </div>
-            ))}
-          </Map>
+          />
         </div>
+      )}
+
+      {mapEnabled &&
+        isBrowser && (
+          <div className={styles.mapbox}>
+            <Map
+              style={mapboxTheme}
+              containerStyle={{
+                height: '100vh',
+                width: '100%'
+              }}
+              center={[focusLocation[0], focusLocation[1]]}
+              zoom={[10]}
+              pitch={[30]}
+            >
+              {!loading &&
+                recommendations.length > 0 &&
+                recommendations.map((recommendation, index) => (
+                  <div key={recommendation.lon}>
+                    <Marker
+                      coordinates={{
+                        lng: recommendation.lon,
+                        lat: recommendation.lat
+                      }}
+                      className={styles.mapMarker}
+                    />
+
+                    <Popup
+                      offset={[0, 0]}
+                      coordinates={{
+                        lng: recommendation.lon,
+                        lat: recommendation.lat
+                      }}
+                    >
+                      <button
+                        onClick={() => openDetails(recommendation)}
+                        className={styles.popupContent}
+                      >
+                        <div className={styles.colorStrip} />
+                        <div className={styles.popupContent2}>
+                          <div className={styles.popupIndex}> {index + 1}</div>
+
+                          <div className={styles.popupInner}>
+                            {recommendation.name}
+                          </div>
+                        </div>
+                      </button>
+                    </Popup>
+                  </div>
+                ))}
+            </Map>
+          </div>
       )}
     </div>
   )
