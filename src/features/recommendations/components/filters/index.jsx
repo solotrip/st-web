cdfa6b0f3210b069a37e49ae976ee1da7f6d44de
Berk Durmus <berkdurmus@yahoo.com>
2022-02-22 02:58:44 +0300

    filter list accordion height bug

--- src/features/recommendations/components/filters/index.jsx
+++ src/features/recommendations/components/filters/index.jsx
@@ -5,38 +5,31 @@ import { Accordion } from 'components'
 import styles from './filters.module.scss'
 import { RECENT_FILTERS_CATEGORY } from 'constants/index'
 
-const Filters = ({
-  filters,
-  filterValues,
-  error,
-  loading,
-  updateFilter
-}) => {
-  const filtersByCategory = useMemo(
-    () => _.groupBy(filters, 'category'),
-    [filters])
+const Filters = ({ filters, filterValues, error, loading, updateFilter }) => {
+  const filtersByCategory = useMemo(() => _.groupBy(filters, 'category'), [
+    filters
+  ])
   return (
     <div className={styles.filters}>
-      {
-        Object.keys(filtersByCategory)
-          .map(category => (
-          <Accordion
-            name={category}
-            key={`category-${category}`}
-            expandedDefault={category === RECENT_FILTERS_CATEGORY}
-          >
-            {
-              filtersByCategory[category].map(filter => (
-                <Filter
-                  filter={filter}
-                  value={filterValues[filter.uuid]}
-                  key={`f-${filter.uuid}`}
-                  updateFilter={updateFilter}
-                />))
-            }
-          </Accordion>
-          ))}</div>)
-
+      {Object.keys(filtersByCategory).map(category => (
+        <Accordion
+          className={styles.accordionHolder}
+          name={category}
+          key={`category-${category}`}
+          expandedDefault={category === RECENT_FILTERS_CATEGORY}
+        >
+          {filtersByCategory[category].map(filter => (
+            <Filter
+              filter={filter}
+              value={filterValues[filter.uuid]}
+              key={`f-${filter.uuid}`}
+              updateFilter={updateFilter}
+            />
+          ))}
+        </Accordion>
+      ))}
+    </div>
+  )
 }
 
 export default Filters
