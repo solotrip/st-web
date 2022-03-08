c40561f9207532ef20893d74768af59dadafcaba
Berk Durmus <berkdurmus@yahoo.com>
2022-03-08 03:46:29 +0300

    High res Placeholder

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,7 @@
 /* eslint-disable max-len */
 export const SOLOTRIP_V1 = path => process.env.REACT_APP_SOLOTRIP_API_V1 + path
 export const IMAGE_BASE = process.env.REACT_APP_IMAGE_BASE
-export const LIGHT_IMG_PLACEHOLDER = 'https://ik.imagekit.io/rfjpogvdn8p/lightplaceholder_K7Q0Pfos6.png?ik-sdk-version=javascript-1.4.3'
-export const DARK_IMG_PLACEHOLDER = 'https://ik.imagekit.io/rfjpogvdn8p/darkplaceholder_oP8oGnR_A.png?&updatedAt=1646003262784'
+export const LIGHT_IMG_PLACEHOLDER =
+  'https://ik.imagekit.io/rfjpogvdn8p/placeholder-hires-light_4ML-ByAEd4y.png?ik-sdk-version=javascript-1.4.3&updatedAt=1646700305341'
+export const DARK_IMG_PLACEHOLDER =
+  'https://ik.imagekit.io/rfjpogvdn8p/placeholder-hi-res_BTTX0_l8A.png?ik-sdk-version=javascript-1.4.3&updatedAt=1646699855668'
