3e34383fb0de50d21a81176d568a953f9d50d528
Berk Durmus <berkdurmus@yahoo.com>
2022-04-16 17:55:06 +0300

    update placeholder images

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,6 +2,6 @@
 export const SOLOTRIP_V1 = path => process.env.REACT_APP_SOLOTRIP_API_V1 + path
 export const IMAGE_BASE = process.env.REACT_APP_IMAGE_BASE
 export const LIGHT_IMG_PLACEHOLDER =
-  'https://ik.imagekit.io/rfjpogvdn8p/placeholder-hires-light_4ML-ByAEd4y.png?ik-sdk-version=javascript-1.4.3&updatedAt=1646700305341'
+  'https://ik.imagekit.io/stmedia/light-placeholder_8uPtN0fkr.png?ik-sdk-version=javascript-1.4.3&updatedAt=1650120639808'
 export const DARK_IMG_PLACEHOLDER =
-  'https://ik.imagekit.io/rfjpogvdn8p/placeholder-hi-res_BTTX0_l8A.png?ik-sdk-version=javascript-1.4.3&updatedAt=1646699855668'
+  'https://ik.imagekit.io/stmedia/dark-placeholder_Mq35TBWKI.png?ik-sdk-version=javascript-1.4.3&updatedAt=1650120653833'
