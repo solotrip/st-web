6d924a64a1575201df1c5ced3df7bff5df954d7a
Berk Durmus <berkdurmus@yahoo.com>
2022-05-04 06:20:56 +0300

    browse card layout fix

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './app'
 
-
 if (module.hot) {
   module.hot.accept('./app', () => {
     const NextApp = require('./app').default
-    render(<NextApp/>, document.getElementById('root'))
+    render(<NextApp />, document.getElementById('root'))
   })
 }
-render(<App/>, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
