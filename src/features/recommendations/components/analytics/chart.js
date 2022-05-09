e52f11fb1254bb9e3e97f5a8a23c7fe9602210af
Berk Durmus <berkdurmus@yahoo.com>
2022-05-10 00:09:47 +0300

    filter out areas with empty data on charts

--- src/features/recommendations/components/analytics/chart.js
+++ src/features/recommendations/components/analytics/chart.js
@@ -4,17 +4,14 @@ import { chartConfig } from './config'
 import Chart from 'components/chart/chart'
 import styles from './chart.module.scss'
 const AnalyticsChart = ({ recommendations, type, title }) => {
-
   const chartData = recommendations.map((recommendation, k) => {
-
     const min = _get(recommendation, chartConfig[type].min)
     const max = _get(recommendation, chartConfig[type].max)
 
     const image = recommendation.area_has_image
       ? `https://ik.imagekit.io/stmedia/areas/${recommendation.sid}?tr=w-700,h-550`
-      : (_get(recommendation, 'events[0].images[0]') &&
-        `https://ik.imagekit.io/stmedia/${recommendation.events[0].images[0]}`)
-
+      : _get(recommendation, 'events[0].images[0]') &&
+        `https://ik.imagekit.io/stmedia/${recommendation.events[0].images[0]}`
 
     return {
       name: '#' + (k + 1) + ' ' + recommendation.name,
@@ -27,16 +24,16 @@ const AnalyticsChart = ({ recommendations, type, title }) => {
     }
   })
 
+  const filteredChartData = chartData.filter(
+    d => d.min !== null && d.max !== null
+  )
 
-  return <div className={styles.container}>
-    <Chart
-    data={chartData}
-    type={type}
-    DOMroot={type}
-    />
-  </div>
-
+  return (
+    <div className={styles.container}>
+      {console.log('chart data is here:', filteredChartData)}
+      <Chart data={filteredChartData} type={type} DOMroot={type} />
+    </div>
+  )
 }
-
 
 export default AnalyticsChart
