f3673354009e681ce80f87705634469ce72f7257
Berk Durmus <berkdurmus@yahoo.com>
2022-05-10 15:36:28 +0300

    fix: scroll action on touch devices

--- src/features/auth/routes.jsx
+++ src/features/auth/routes.jsx
@@ -3,24 +3,36 @@ import { NotAuthenticatedRoute, SheetWrapper } from 'components'
 import LoginContainer from 'features/auth/containers/login'
 import SignupContainer from 'features/auth/containers/signup'
 import { Switch } from 'react-router-dom'
+import SettingsSection from 'components/settings-section'
 
 const Routes = () => (
   <Switch>
-    <NotAuthenticatedRoute path="/:path/login" children={
-      <SheetWrapper><LoginContainer/></SheetWrapper>
-    }
+    <NotAuthenticatedRoute
+      path="/:path/login"
+      children={
+        <SheetWrapper>
+          <SheetWrapper.Content>
+            <SettingsSection>
+              <LoginContainer />{' '}
+            </SettingsSection>
+          </SheetWrapper.Content>
+        </SheetWrapper>
+      }
     />
-    <NotAuthenticatedRoute path="/:path/signup" children={
-      <SheetWrapper><SignupContainer authWall/></SheetWrapper>}
+    <NotAuthenticatedRoute
+      path="/:path/signup"
+      children={
+        <SheetWrapper>
+          <SheetWrapper.Content>
+            <SettingsSection>
+              <SignupContainer authWall />
+            </SettingsSection>
+          </SheetWrapper.Content>
+        </SheetWrapper>
+      }
     />
-    <NotAuthenticatedRoute path="/login" children={
-      <LoginContainer/>
-    }
-    />
-    <NotAuthenticatedRoute path="/signup" children={
-      <SignupContainer/>
-    }
-    />
+    <NotAuthenticatedRoute path="/login" children={<LoginContainer />} />
+    <NotAuthenticatedRoute path="/signup" children={<SignupContainer />} />
   </Switch>
 )
 export default Routes
