949912a601542a54af10d164d47e2635e90ca342
Berk Durmus <berkdurmus@yahoo.com>
2022-06-01 02:32:24 +0300

    fix: share sheet

--- src/features/recommendations/containers/share/list.jsx
+++ src/features/recommendations/containers/share/list.jsx
@@ -125,130 +125,128 @@ const ShareListContainer = () => {
   }
 
   return (
-    <div className={styles.what}>
-      <SheetWrapper disableDrag={false} closable={true}>
-        {' '}
-        <SheetWrapper.Content>
-          <Loader loading={recommendations_loading || !url || recommendations_error}>
-            <SettingsSection title="Share" description="Share this recommendation via">
-              <div className={styles.shareHolder}>
-                <button className={styles.copyButton} onClick={copyToClipboard}>
-                  {url} <MdContentCopy />
-                </button>
-                <a
-                  href={`mailto:?subject=Travel recommendation for you on Pulfy&body=${createDynamicURL(
-                    'mail'
-                  )}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <EmailIcon size={60} round={false} />
-                  <div className={styles.shareText}>Email</div>
-                </a>
-                <a
-                  href={`https://wa.me/?text=${shortUrl}`}
-                  className={styles.shareButton}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <WhatsappIcon size={60} round={false} />
-                  <div className={styles.shareText}>Whatsapp</div>
-                </a>
-                <a
-                  href={`https://t.me/share/url?url=${createDynamicURL('telegram').url}&text=${
-                    createDynamicURL('telegram').text
-                  }`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <TelegramIcon size={60} round={false} />
-                  <div className={styles.shareText}>Telegram</div>
-                </a>
-                <a
-                  href={`fb-messenger://share/?link=${createDynamicURL('facebook').url}&app_id=${
-                    process.env.REACT_APP_FACEBOOK_APP_ID
-                  }`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <FacebookMessengerIcon size={60} round={false} />
-                  <div className={styles.shareText}>Messenger</div>
-                </a>
+    <SheetWrapper disableDrag={false} closable={true}>
+      {' '}
+      <SheetWrapper.Content>
+        <Loader loading={recommendations_loading || !url || recommendations_error}>
+          <SettingsSection title="Share" description="Share this recommendation via">
+            <div className={styles.shareHolder}>
+              <button className={styles.copyButton} onClick={copyToClipboard}>
+                {url} <MdContentCopy />
+              </button>
+              <a
+                href={`mailto:?subject=Travel recommendation for you on Pulfy&body=${createDynamicURL(
+                  'mail'
+                )}`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <EmailIcon size={60} round={false} />
+                <div className={styles.shareText}>Email</div>
+              </a>
+              <a
+                href={`https://wa.me/?text=${shortUrl}`}
+                className={styles.shareButton}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <WhatsappIcon size={60} round={false} />
+                <div className={styles.shareText}>Whatsapp</div>
+              </a>
+              <a
+                href={`https://t.me/share/url?url=${createDynamicURL('telegram').url}&text=${
+                  createDynamicURL('telegram').text
+                }`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <TelegramIcon size={60} round={false} />
+                <div className={styles.shareText}>Telegram</div>
+              </a>
+              <a
+                href={`fb-messenger://share/?link=${createDynamicURL('facebook').url}&app_id=${
+                  process.env.REACT_APP_FACEBOOK_APP_ID
+                }`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <FacebookMessengerIcon size={60} round={false} />
+                <div className={styles.shareText}>Messenger</div>
+              </a>
 
-                <a
-                  href={`https://www.reddit.com/r/travel/submit?url=${
-                    createDynamicURL('reddit').url
-                  }&title=${createDynamicURL('reddit').text}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <RedditIcon size={60} round={false} />
-                  <div className={styles.shareText}>Reddit r/travel</div>
-                </a>
+              <a
+                href={`https://www.reddit.com/r/travel/submit?url=${
+                  createDynamicURL('reddit').url
+                }&title=${createDynamicURL('reddit').text}`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <RedditIcon size={60} round={false} />
+                <div className={styles.shareText}>Reddit r/travel</div>
+              </a>
 
-                <a
-                  href={`https://twitter.com/intent/tweet?text=${
-                    createDynamicURL('twitter').text
-                  }&url=${createDynamicURL('twitter').url}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <TwitterIcon size={60} round={false} />
-                  <div className={styles.shareText}>Twitter</div>
-                </a>
+              <a
+                href={`https://twitter.com/intent/tweet?text=${
+                  createDynamicURL('twitter').text
+                }&url=${createDynamicURL('twitter').url}`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <TwitterIcon size={60} round={false} />
+                <div className={styles.shareText}>Twitter</div>
+              </a>
 
-                <a
-                  href={`https://www.facebook.com/dialog/share?
+              <a
+                href={`https://www.facebook.com/dialog/share?
                 app_id=1042368969717573&display=popup&href=${url}&redirect_uri=${url}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <FacebookIcon size={60} round={false} />
-                  <div className={styles.shareText}>Facebook</div>
-                </a>
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <FacebookIcon size={60} round={false} />
+                <div className={styles.shareText}>Facebook</div>
+              </a>
 
-                <a
-                  href={`http://pinterest.com/pin/create/link/?url=${url}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <PinterestIcon size={60} round={false} />
-                  <div className={styles.shareText}>Pinterest</div>
-                </a>
+              <a
+                href={`http://pinterest.com/pin/create/link/?url=${url}`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <PinterestIcon size={60} round={false} />
+                <div className={styles.shareText}>Pinterest</div>
+              </a>
 
-                <a
-                  href={`https://line.me/R/msg/text/?${shortUrl}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <LineIcon size={60} round={false} />
-                  <div className={styles.shareText}>Line</div>
-                </a>
+              <a
+                href={`https://line.me/R/msg/text/?${shortUrl}`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <LineIcon size={60} round={false} />
+                <div className={styles.shareText}>Line</div>
+              </a>
 
-                <a
-                  href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.shareButton}
-                >
-                  <LinkedinIcon size={60} round={false} />
-                  <div className={styles.shareText}>Linkedin</div>
-                </a>
-              </div>
-            </SettingsSection>
-          </Loader>
-        </SheetWrapper.Content>
-        <SheetWrapper.Footer onClick={onBack} text="Close" disabled={false} />
-      </SheetWrapper>
-    </div>
+              <a
+                href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.shareButton}
+              >
+                <LinkedinIcon size={60} round={false} />
+                <div className={styles.shareText}>Linkedin</div>
+              </a>
+            </div>
+          </SettingsSection>
+        </Loader>
+      </SheetWrapper.Content>
+      <SheetWrapper.Footer onClick={onBack} text="Close" disabled={false} />
+    </SheetWrapper>
   )
 }
 
